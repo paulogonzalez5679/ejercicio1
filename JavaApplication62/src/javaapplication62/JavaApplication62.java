cb3440d9c94def89cc2837d42eaf493aea2e855a
paulogonzalez5679 <33768253+paulogonzalez5679@users.noreply.github.com>
2017-11-17 19:35:06 -0500

    Update JavaApplication62.java

--- JavaApplication62/src/javaapplication62/JavaApplication62.java
+++ JavaApplication62/src/javaapplication62/JavaApplication62.java
@@ -19,6 +19,7 @@ public class JavaApplication62 {
     public static void main(String[] args) {
         // TODO code application logic here
         JOptionPane.showMessageDialog(null, "HOLA MUNDO");
+        JOptionPane.showMessageDialog(null, "ADIOS");
     }
     
 }
